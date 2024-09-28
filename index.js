82d789f17c777693a9e3d3213eccee28fc78f812
chris <Mileschris466@gmail.com>
2024-09-28 03:29:42 +0300

    arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
 
 // Write your code here
+
+const num1 = 2;
+
+const num2 = 31;
+
+const multiply = (num1 * num2);
+
+console.log(multiply);
+
+ let num3 = 10;
+ let num4= 6;
+
+ let mod = (num3 % num4);
+
+ console.log(mod);
+
+ let people = [20,4,13,6,18];
+
+ let max = Math.max(...people);
+
+ console.log(max);
+
+
+
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+ console.log(random);
+
+
+
+ 
+
+
+
+
+
+
